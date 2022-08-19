831ea71ddb4579dca5b5aaa07d73f1ca666b6539
Chandranshh <chandranshyay@gmail.com>
2022-08-19 15:48:56 +0530

    used useState() STATE property to change the state of the item name whenever the button is pressed

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,17 +1,24 @@
+import { useState } from "react";
 import ExpenseDate from "./ExpenseDate";
 import Card from "../UI/Card";
 import "./ExpenseItem.css";
 
-
 //Date is passed in form of props from App.js
-const ExpenseItem = ({date, title, amount}) => {
+const ExpenseItem = ({ date, title, amount }) => {
+
+  const [newTitle, setnewTitle] = useState(title);
+  function clickHandler() {
+    setnewTitle("Changed title");
+  }
+
   return (
     <Card className="expense-item">
       <ExpenseDate date={date} />
       <div className="expense-item__description">
-        <h2>{title}</h2>
+        <h2>{newTitle}</h2>
         <div className="expense-item__price">${amount}</div>
       </div>
+      <button onClick={clickHandler}>Change Title</button>
     </Card>
   );
 };
